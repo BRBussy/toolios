e1ee87bb6e4e26045585576407106098d62182ad
Bernard Bussy <brbitzbussy@gmail.com>
2021-06-10 19:28:16 +0200

    ready for the runsies

--- cmd/gcbBucketInit/main.go
+++ cmd/gcbBucketInit/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"context"
 	"github.com/BRBussy/toolios/internal/pkg/logs"
+	"github.com/BRBussy/toolios/pkg/storage"
+	"github.com/rs/zerolog/log"
+	"time"
 )
 
 const bucketName = "sol-ar-world"
 
 func main() {
 	logs.Setup(nil)
+
+	ctx, cancelFunc := context.WithTimeout(
+		context.Background(),
+		10*time.Second,
+	)
+	defer cancelFunc()
+
+	if _, err := storage.NewGCSProvider(
+		ctx,
+		bucketName,
+		map[string]bool{},
+	); err != nil {
+		log.Fatal().Err(err).Msg("error constructing storage provider")
+	}
 }
